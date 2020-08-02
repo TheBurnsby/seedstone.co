387d1252b6a9bd3fdf51ac178c74f133c494c699
Jon Burns <jonburns10@gmail.com>
2020-08-01 23:26:10 -0700

    site build

---
+++ src/index.js
@@ -0,0 +1,8 @@
+(function() { 'use strict';
+    var font = document.createElement('link');
+    font.async = true;
+    font.rel = 'stylesheet';
+    font.href = 'https://fonts.googleapis.com/css?family=Barlow+Condensed:wght@300;400;800';
+    document.head.appendChild(font);
+}());
+// <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;800&display=swap" rel="stylesheet">
